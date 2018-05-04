8d49fc5609a12a81f64477899b4e97d07efe2b27
Jesse Aguirre <jesse@Jesses-MacBook-Pro.local>
2018-05-04 16:35:24 -0700

    NavBar

--- config/passport.js
+++ config/passport.js
@@ -10,12 +10,20 @@ passport.use(new GoogleStrategy({
     User.findOne({ 'googleId': profile.id }, function(err, user) {
       if (err) return cb(err);
       if (user) {
-        return cb(null, user);
-      } else {
+        if (!user.avatar) {
+            user.avatar = profile.photos[0].value;
+            user.save(function(err) {
+                return cb(null, user);
+            });
+        } else {
+            return cb(null, user);
+        }
+    } else {
         // we have a new User via OAuth!
         var newUser = new User({
           name: profile.displayName,
           email: profile.emails[0].value,
+          avatar: profile.photos[0].value,
           googleId: profile.id
         });
         newUser.save(function(err) {
