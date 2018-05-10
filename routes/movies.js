3ff24fbe736859dc6344fe568eac42da4e28c0cb
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-05-10 14:29:26 -0700

    users must now be logged in to do various things

--- routes/movies.js
+++ routes/movies.js
@@ -4,10 +4,16 @@ var moviesCtrl = require('../controllers/moviesController');
 
 router.get('/', moviesCtrl.nowShowing);
 router.get('/search', moviesCtrl.searchMovies);
-router.get('/recs', moviesCtrl.recs);
-router.get('/:id', moviesCtrl.getMovie);
-router.get('/favorites/:id', moviesCtrl.addFavorite);
-router.delete('/favorites/:id', moviesCtrl.delFavorite);
-router.post('/:id/comments', moviesCtrl.addComment);
-router.delete('/:apiId/comments/:id', moviesCtrl.delComment);
+router.get('/recs', isLoggedIn, moviesCtrl.recs);
+router.get('/:id', isLoggedIn, moviesCtrl.getMovie);
+router.get('/favorites/:id', isLoggedIn, moviesCtrl.addFavorite);
+router.delete('/favorites/:id', isLoggedIn, moviesCtrl.delFavorite);
+router.post('/:id/comments', isLoggedIn, moviesCtrl.addComment);
+router.delete('/:apiId/comments/:id', isLoggedIn, moviesCtrl.delComment);;;
+
+function isLoggedIn(req, res, next) {
+    if ( req.isAuthenticated() ) return next();
+    res.redirect('/auth/google');
+}
+
 module.exports = router;
